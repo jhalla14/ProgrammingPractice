344c31918de932df9ae05905e90abbbfda0fd550
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2014-01-13 14:38:12 -0500

    successful implementation of a linked list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Joshua Hall
@@ -9,8 +11,17 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Test");
+        Node list = new Node(2);
 
+        list.appendToTail(3);
+        list.appendToTail(4);
 
+        while (list.next != null){
+            System.out.println(list.data);
+            list = list.next;
+        }
+        if (list.next == null){
+            System.out.println(list.data);
+        }
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -6,23 +6,34 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Node {
-    Node next = null;
-    int data;
+    Node next = null; //pointer to next element in list
+    int data;          //holds data in that element of the list
+
 
     public Node(int data){
         this.data = data;
     }
 
     public void appendToTail(int data){
-        Node end = new Node(data);
-        Node n = this;
+        Node end = new Node(data);  //inserting a new Node at the end
+        Node n = this;  //current Node instance
 
         while(n.next != null){
-           n = n.next;
+           n = n.next;  //move to the next pointer
         }
 
-        n.next = end;
+        n.next = end;  //make the last one the new end Node
     }
 
+    @Override
+    public String toString() {
 
+//        Node n = this;
+//        while(n.next != null){
+//            System.out.println(this.data);
+//            n = n.next;
+//        }
+
+        return super.toString();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
